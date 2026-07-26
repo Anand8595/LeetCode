19e609a470f3174ed253df0e8fdf24167ec23c0d
Anand Ahire <anandsahire8595@gmail.com>
2026-07-26 09:02:49 +0530

    Added solution - Time: O(size), Space: O(1)

---
+++ 701889-missing-in-array-GFG/701889-missing-in-array.c
@@ -0,0 +1,35 @@
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int missingNum(int arr[], int size) {
+    int n = size + 1;
+    int i = 0;
+
+    while (i < size) {
+        if (arr[i] == i + 1 || arr[i] == n) {
+            i++;
+        } else {
+            int idx = arr[i] - 1;
+
+            if (arr[i] != arr[idx]) {
+                swap(arr, i, idx);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    for (i = 0; i < size; i++) {
+        if (arr[i] != i + 1)
+            return i + 1;
+    }
+
+    return n;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
